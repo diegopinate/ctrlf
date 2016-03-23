832df0c498aca018cda7af3a7c0aeabbbb6d1c64
Diego <ds635459@sju.edu>
2016-03-23 01:38:29 -0400

    Added login page and edited some stuff

--- src/ctrlfApp.js
+++ src/ctrlfApp.js
@@ -1,1 +1,12 @@
-var app = angular.module('ctrlfApp', [])
+/**
+Controller for Ctrl-F app.
+
+*/
+var app = angular.module('ctrlfApp', []);
+app.controller('mainPageController', function($scope,$http){
+	
+	
+	
+	
+	
+});
